0617a28456710172330025dd98b515fe403de5c3
Xiaochuan Ma <mxcins@gmail.com>
2019-04-18 18:52:43 +0800

    created_at

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,4 +1,6 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
