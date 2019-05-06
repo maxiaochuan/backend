4f94f584748cad7e6f0089246e5eb26ff754ddde
Xiaochuan Ma <mxcins@gmail.com>
2019-05-06 16:07:17 +0800

    update

--- app/graphql/types/micropost_type.rb
+++ app/graphql/types/micropost_type.rb
@@ -2,6 +2,6 @@ module Types
   class MicropostType < Types::BaseObject
     field :id, ID, null: false
     field :content, String, null: true
-    field :user, [Types::UserType], null: true
+    field :user, UserType, null: true
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,18 @@
 module Types
   class QueryType < Types::BaseObject
     field :user, UserType, null: true do
-      description "Find user by ID"
+      description "Find User by ID"
       argument :id, ID, required: true
     end
 
     field :users, [UserType], null: true do
       description "All Users"
+    end
+
+    field :microposts, [MicropostType], null: true do
+      description "All Microposts"
+      argument :id, ID, required: false
+      argument :user_id, ID, required: false
     end
 
     def user id:
@@ -15,6 +21,10 @@ module Types
 
     def users
       User.all()
+    end
+
+    def microposts options = {}
+      Micropost.where options
     end
   end
 end
