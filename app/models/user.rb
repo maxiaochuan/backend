d76102601d26969ac00368ce7365e9caad6a56c3
Xiaochuan Ma <mxcins@gmail.com>
2019-05-07 13:27:00 +0800

    add user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEXP },
                     uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new name: 'xiaochuan', email: 'mxcins@gmail.com', password: '123456'
+    @user = User.new name: 'xiaochuan', email: 'mxcins@gmail.com', password: '123456', password_confirmation: '123456'
   end
 
   test 'should be valid' do
@@ -35,5 +35,15 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
