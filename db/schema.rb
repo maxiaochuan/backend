4216221da2bac9ba223e16d52bb6a7450f306fda
Xiaochuan Ma <mxcins@gmail.com>
2019-05-07 12:36:37 +0800

    update user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_083557) do
+ActiveRecord::Schema.define(version: 2019_05_07_040714) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
